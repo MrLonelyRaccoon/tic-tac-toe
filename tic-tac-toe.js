d150853dd93a3cd8e5e1342badccc0514aa15612
Diego Centeno <diego.raccoon@gmail.com>
2016-11-17 10:19:48 -0800

    tic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,13 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+var turn= "player1"
+function makeMove() {
+  if (turn== "player1"){
+//code to make a move as Player1
+turn="player2"
+} elsw {
+//code to make a move as player2
+turn= "player1"
+ }
+}
